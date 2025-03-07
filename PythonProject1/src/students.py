abeee936ea2dbda6fe2e37d9841a3fa11ea9f1ac
aborgula <132065514+aborgula@users.noreply.github.com>
2025-03-07 17:49:33 +0100

    Update students.py

--- PythonProject1/src/students.py
+++ PythonProject1/src/students.py
@@ -11,74 +11,58 @@ class StudentManagement:
             self.students[id] = {"name": name, "age": age}
             return True
 
-        """
-        Dodaje nowego studenta do bazy danych.
-
-        Args:
-            name: Imie studenta.
-            age: Wiek studenta.
-            id: Unikalny identyfikator studenta.
-
-        Returns:
-            True, jesli dodanie zakonczylo sie sukcesem.
-            False w przeciwnym wypadku.
-
-        if id is None or name is None or age is None:
-            return False"""
- #       pass  # Implementacja dodawania studenta
 
 
     def update_student(self, id: str, name: str, age: int) -> bool:
-        """
-        Aktualizuje dane istniejacego studenta na podstawie identyfikatora.
+        
+        if id is None or name is None or age is None:
+            return False
+        
+        if id in self.students:
+            self.students[id]["name"] = name
+            self.students[id]["age"] = age
+            return True
+        
 
-        Args:
-            name: Imie studenta.
-            age: Wiek studenta.
-            id: Unikalny identyfikator studenta.
-
-        Returns:
-            True, jesli aktualizacja zakonczyla sie sukcesem.
-            False w przeciwnym wypadku.
-        """
-        pass  # Implementacja aktualizacji studenta
-
-    def remove_student(id: str, self) -> bool:
-        """
-        Usuwa studenta z bazy danych na podstawie jego identyfikatora.
-
-        Args:
-            id: Unikalny identyfikator studenta.
-
-        Returns:
-            True, jesli usuniecie zakonczylo sie sukcesem.
-            False w przeciwnym wypadku.
-        """
-        pass  # Implementacja usuwania studenta
+    def remove_student(self, id: str) -> bool:
+        
+        if id in self.students:
+            del self.students[id]
+            return True
+        
+        return False
+        
 
     def add_grade(self, student_id: str, subject: str, grade: float) -> bool:
-        """
-        Dodaje ocene z danego przedmiotu dla okreslonego studenta.
-
-        Args:
-            student_id: Unikalny identyfikator studenta.
-            subject: Nazwa przedmiotu.
-            grade: Ocena.
-
-        Returns:
-            True, jesli dodanie oceny zakonczylo sie sukcesem (2.0, 3.0, 3.5, 4.0, 4.5, 5.0), 
-            False w przeciwnym razie.
-        """
-        pass  # Implementacja dodawania oceny
+        
+        grades = {2.0, 3.0, 4.0, 5.0}
+        
+        if student_id not in self.students or grade not in grades:
+            return False
+        
+        if "grades" not in self.students[student_id]:
+            self.students[student_id]["grades" ] = {}
+            
+        if subject not in self.students[student_id]["grades"]:
+            self.students[student_id]["grades"][subject] = []
+            
+        self.students[student_id]["grades"][subject].append(grade)
+        return True
+        
 
     def avg_grades(self, subject: str) -> float:
-        """
-        Oblicza srednia ocen z danego przedmiotu dla wszystkich studentow.
-
-        Args:
-            subject: Nazwa przedmiotu.
-
-        Returns:
-            Srednia ocen z przedmiotu jako liczba zmiennoprzecinkowa.
-        """
-        pass  # Implementacja obliczania sredniej ocen
+        
+        sum = 0
+        amount = 0
+        for student_id in self.students:
+            for grade in self.students[student_id]["grades"][subject]:
+                sum += grade
+                amount += 1
+        
+        return sum/amount
+    
+    
+    def students(self):
+        return self.students
+    
+    
